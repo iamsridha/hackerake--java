f151fb1aaa7d4240aabb3ef943935b677c165d98
iamsridha <124337210+iamsridha@users.noreply.github.com>
2023-02-07 10:45:00 +0530

    Add files via upload

---
+++ hello_world.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+public class hello_world {
+    public static void main(String[] args){
+     System.out.println("Hello, World.");
+      System.out.println("Hello, Java.");   
+    }
+}
